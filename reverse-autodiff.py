097a2d8f8dd482444cebaeabe72b68a1b0040eaf
Tom White <tom@cloudera.com>
2017-11-14 15:18:36 +0000

    Get reverse working correctly for 3 cases.

--- reverse-autodiff.py
+++ reverse-autodiff.py
@@ -75,7 +75,6 @@ def reverse_autodiff(nodes, inputs, outputs, input_values):
 	g = Graph(nodes)
 	
 	# Forward pass
-	
 	for (var, val) in zip(inputs, input_values):
 		var.set(val)
 	for n in outputs:
@@ -90,11 +89,17 @@ def reverse_autodiff(nodes, inputs, outputs, input_values):
 	def build_grad(v, g, grad_table):
 		if v in grad_table:
 			return grad_table[v]
+		#print("build_grad for %s" % v.name)
 		grad = 0
 		for c in g.get_consumers(v):
+			#print("consumer %s with inputs %s" % (c.name, [i.name for i in g.get_inputs(c)]))
 			d = build_grad(c, g, grad_table)
-			grad += sum([grad_op(input.eval()) * d for (grad_op, input) in zip(c.grad_ops, g.get_inputs(c))])
+			#print("\tgrad_ops %s" % (c.grad_ops,))
+			c_inputs = g.get_inputs(c)
+			index = c_inputs.index(v) # the index of v in the inputs to c
+			grad += c.grad_ops[index](*[input.eval() for input in c_inputs]) * d
 		grad_table[v] = grad
+		#print("grad_table for %s is %s" % (v.name, grad))
 		return grad_table[v]
 
 	for v in inputs:
@@ -106,23 +111,24 @@ def autodiff(nodes, inputs, outputs):
 	return lambda input_values: reverse_autodiff(nodes, inputs, outputs, input_values)
 
 # tanh(x)
-n1 = Variable("n1", None, None, None)
+n0 = Variable("n0", None, None, None)
+n1 = Variable("n1", (n0, ), lambda x: x, (lambda x: 1,))
 n2 = Variable("n2", (n1, ), lambda x: math.tanh(x), (lambda x: 1 / (math.cosh(x) * math.cosh(x)),))
 
-fdash = autodiff((n1, n2), (n1,), (n2,))
+fdash = autodiff((n1, n2), (n0,), (n2,))
 print(fdash((1,)))
 
 # x * tanh(x)
-n3 = Variable("n3", None, None, None)
-n4 = Variable("n4", (n2, n3), lambda x, y: x * y, (lambda x: x, lambda x: x))
+n3 = Variable("n3", (n0, ), lambda x: x, (lambda x: 1,))
+n4 = Variable("n4", (n2, n3), lambda x, y: x * y, (lambda x, y: y, lambda x, y: x))
 
-fdash = autodiff((n1, n2, n3, n4), (n1, n3), (n4,))
-print(fdash((1, 1)))
+fdash = autodiff((n0, n1, n2, n3, n4), (n0,), (n4,))
+print(fdash((1,)))
 
 # x * x
-n1 = Variable("n1", None, None, None)
-n2 = Variable("n2", None, None, None)
-n3 = Variable("n3", (n1, n2), lambda x, y: x * y, (lambda x: x, lambda x: x))
+n1 = Variable("n1", (n0, ), lambda x: x, (lambda x: 1,))
+n2 = Variable("n2", (n0, ), lambda x: x, (lambda x: 1,))
+n3 = Variable("n3", (n1, n2), lambda x, y: x * y, (lambda x, y: y, lambda x, y: x))
 
-fdash = autodiff((n1, n2, n3), (n1, n2), (n3,))
-print(fdash((1, 1)))
+fdash = autodiff((n1, n2, n3), (n0,), (n3,))
+print(fdash((1,)))
